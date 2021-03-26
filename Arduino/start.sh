92e104ce71a9ee28f741fa655a35a9007cef5eb0
Joshua Neizer <joshua.neizer@gmail.com>
2021-03-25 22:43:57 -0400

    Updated to match new library

--- Arduino/start.sh
+++ Arduino/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+alias 'arduino-cli'='~/go/src/github.com/arduino/arduino-cli/bin/arduino-cli'
+
+# Updates the arduino code
+python3 update-arduino.py
+
 # Compiles and verifies code
-amake -v
+arduino-cli compile --fqbn arduino:avr:uno run/
 
 # Uploads and runs code to the arduino
-amake -u
-
-
+arduino-cli upload -p /dev/ttyACM0 --fqbn arduino:avr:uno run/
