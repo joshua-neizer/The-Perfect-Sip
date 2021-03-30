9798b54f11537b5b26bb72df960a44ea300a6931
joshua-neizer <joshua.neizer@gmail.com>
2021-03-29 22:25:59 -0400

    Allows to host locally

--- App/main.py
+++ App/main.py
@@ -5,5 +5,5 @@ app =  create_app()
 #if main is run, the line will be executed
 if __name__ == '__main__':
     #run flask application and start up web server
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
 
